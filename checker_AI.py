e6590daf6c9ccee690c3673e51316525ce080f8e
omarelders <118695915+omarelders@users.noreply.github.com>
2025-04-30 20:28:44 +0300

    Update checker_AI.py

--- checker_AI.py
+++ checker_AI.py
@@ -187,29 +187,6 @@ def getNextBoard(game : Game, move : full_move) -> Game:
 
 
 
-def simpleEvaluationFunction(game: Game, maximizingPlayer: bool) -> float:
-    score = 0
-    if game.is_over():
-        if game.get_winner() is None:
-            return -2 if maximizingPlayer else 2
-        elif game.get_winner() == 1:
-            return 9999
-        else:
-            return -9999
-
-    for piece in game.board.pieces:
-        if not piece.captured:
-            if piece.player == 1 and piece.king:
-                score += 1.4
-            elif piece.player == 1:
-                score += 1
-            elif piece.player == 2 and piece.king:
-                score -= 1.4
-            elif piece.player == 2:
-                score -= 1
-    return score
-
-
 
 # Function which starts minimax at the current board to the desired depth and backtracks to get the best move
 def minimaxStart(game : Game, depth : int, maximizingPlayer : bool):
